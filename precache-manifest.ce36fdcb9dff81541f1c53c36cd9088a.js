d99e8ade601b09a6413fa6a20cf73a8e78b3715b
Anna Jimenez <>
2020-07-18 01:29:57 -0400

    Updates

---
+++ precache-manifest.ce36fdcb9dff81541f1c53c36cd9088a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd04764f09382a8b3fcbbfe9dcc20454",
+    "url": "/react-deploy-github/index.html"
+  },
+  {
+    "revision": "44be45909a08647c5698",
+    "url": "/react-deploy-github/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "89279df726285becfc19",
+    "url": "/react-deploy-github/static/js/2.a9c19741.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy-github/static/js/2.a9c19741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44be45909a08647c5698",
+    "url": "/react-deploy-github/static/js/main.f60283a3.chunk.js"
+  },
+  {
+    "revision": "168c07fbf666b774be50",
+    "url": "/react-deploy-github/static/js/runtime-main.41b53eac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy-github/static/media/logo.5d5d9eef.svg"
+  }
+]);
